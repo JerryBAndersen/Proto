24f9411265dda4032abc8891fb6d94df0d8a59c1
JerryBAndersen <jerrybandersen@yahoo.de>
2016-03-20 10:07:27 +0100

    apply changes

---
+++ Proto/Misc/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using Proto.Storables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proto.Misc {
+    class Inventory : List<Storable>{
+
+        public Inventory(int capacity) {
+            this.Capacity = capacity;
+        }
+
+        void Transfer<T>(Storable storable, Inventory to) {
+            to.Add(storable);
+            this.Remove(storable);
+        }
+    }
+}
---
+++ Proto/Misc/Offer.cs
@@ -0,0 +1,30 @@
+﻿using Proto.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proto.Misc
+{
+    class Offer
+    {
+        public Entity me, you;
+        public Inventory[] mine, yours;
+        public bool accepted = false;
+
+        public Offer(Entity me, Entity you, Inventory[] mine, Inventory[] yours) {
+            this.me = me;
+            this.you = you;
+            this.mine = mine;
+            this.yours = yours;
+        }
+
+        public void Apply() {
+            if (accepted) {
+
+            }
+        }
+
+    }
+}
